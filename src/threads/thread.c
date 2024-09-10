1bb25d827c166d488836973ecd78a47cc4ca2c29
Kevin S <kevinxanjula@gmail.com>
2024-09-10 21:09:23 +0530

    priority scheduling

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -70,6 +70,7 @@ static void *alloc_frame (struct thread *, size_t size);
 static void schedule (void);
 void thread_schedule_tail (struct thread *prev);
 static tid_t allocate_tid (void);
+bool thread_priority_comp(const struct list_elem *a,const struct list_elem *b, void* hehe UNUSED);
 
 /* Initializes the threading system by transforming the code
    that's currently running into a thread.  This can't work in
@@ -238,6 +239,12 @@ thread_unblock (struct thread *t)
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
   list_push_back (&ready_list, &t->elem);
+  list_sort(&ready_list, thread_priority_comp, NULL);
+  struct thread *thr = list_entry (list_front(&ready_list), struct thread, elem);
+
+  if (thread_current() != idle_thread && thread_current()->priority < thr->priority)
+    thread_yield();
+  
   t->status = THREAD_READY;
   intr_set_level (old_level);
 }
@@ -307,8 +314,10 @@ thread_yield (void)
   ASSERT (!intr_context ());
 
   old_level = intr_disable ();
-  if (cur != idle_thread) 
+  if (cur != idle_thread){
     list_push_back (&ready_list, &cur->elem);
+    list_sort(&ready_list, thread_priority_comp, NULL);
+  }
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
@@ -336,6 +345,7 @@ void
 thread_set_priority (int new_priority) 
 {
   thread_current ()->priority = new_priority;
+  thread_yield();
 }
 
 /* Returns the current thread's priority. */
@@ -582,3 +592,10 @@ allocate_tid (void)
 /* Offset of `stack' member within `struct thread'.
    Used by switch.S, which can't figure it out on its own. */
 uint32_t thread_stack_ofs = offsetof (struct thread, stack);
+
+bool thread_priority_comp(const struct list_elem *a,const struct list_elem *b, void* hehe UNUSED)
+{
+    struct thread* st_a = list_entry(a, struct thread, elem);
+    struct thread* st_b = list_entry(b, struct thread, elem);
+    return st_a->priority > st_b->priority;
+}
